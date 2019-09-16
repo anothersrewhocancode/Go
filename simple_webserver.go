309e4196641a938de9f54559103120bde6a1ec5b
VigneshViswanathan <vigneshwarv@gmail.com>
2019-09-17 00:11:43 +0530

    v2

--- simple_webserver.go
+++ simple_webserver.go
@@ -11,7 +11,7 @@ func index_handler(w http.ResponseWriter, r *http.Request){
   if err != nil {
 	  fmt.Fprintln(w, err)
   }
-  fmt.Fprintf(w, "Served from:" + name + "\n")
+  fmt.Fprintf(w, "This is v2. Served from:" + name + "\n")
 }
 
 func contact_handler(w http.ResponseWriter, r *http.Request){
