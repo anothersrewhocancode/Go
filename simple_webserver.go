00cad720e6d93269064697f286929c98a8c28e3b
VigneshViswanathan <vigneshwarv@gmail.com>
2019-10-03 13:06:54 +0530

    Update simple_webserver.go

--- simple_webserver.go
+++ simple_webserver.go
@@ -11,7 +11,7 @@ func index_handler(w http.ResponseWriter, r *http.Request){
   if err != nil {
 	  fmt.Fprintln(w, err)
   }
-  fmt.Fprintf(w, "This is v16 TESTTTT. Served from:" + name + "\n")
+  fmt.Fprintf(w, "This is v17 TESTTTT. Served from:" + name + "\n")
 }
 
 func contact_handler(w http.ResponseWriter, r *http.Request){
